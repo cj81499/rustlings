9829c16b8d9215241e517c74665e1de4ed7cb829
Cal Jacobson <cj81499@users.noreply.github.com>
2022-10-02 02:40:09 -0500

    work on solutions

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
@@ -23,6 +21,25 @@ struct Team {
     name: String,
     goals_scored: u8,
     goals_conceded: u8,
+}
+
+fn update_score(
+    scores: &mut HashMap<String, Team>,
+    team_name: String,
+    score: u8,
+    opponent_score: u8,
+) {
+    scores
+        .entry(team_name.clone())
+        .and_modify(|t| {
+            t.goals_scored += score;
+            t.goals_conceded += opponent_score;
+        })
+        .or_insert(Team {
+            name: team_name,
+            goals_scored: score,
+            goals_conceded: opponent_score,
+        });
 }
 
 fn build_scores_table(results: String) -> HashMap<String, Team> {
@@ -35,11 +52,9 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be number of goals conceded from team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
+
+        update_score(&mut scores, team_1_name, team_1_score, team_2_score);
+        update_score(&mut scores, team_2_name, team_2_score, team_1_score);
     }
     scores
 }
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,11 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(qty: i32) -> i32 {
+    let per_apple_price = if qty > 40 { 1 } else { 2 };
+    qty * per_apple_price
+}
 
 // Don't modify this function!
 #[test]
