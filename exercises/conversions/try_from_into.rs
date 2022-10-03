543cf26077e78218a38700ea2cad5f177bbfbb0b
Cal Jacobson <cj81499@users.noreply.github.com>
2022-10-03 00:55:26 -0500

    try_from_into

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -38,6 +38,15 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let (red, green, blue) = tuple;
+
+        let (red, green, blue) = (
+            u8::try_from(red).or(Err(IntoColorError::IntConversion))?,
+            u8::try_from(green).or(Err(IntoColorError::IntConversion))?,
+            u8::try_from(blue).or(Err(IntoColorError::IntConversion))?,
+        );
+
+        Ok(Color { red, green, blue })
     }
 }
 
@@ -45,6 +54,7 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        Self::try_from((arr[0], arr[1], arr[2]))
     }
 }
 
@@ -52,6 +62,11 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            Err(IntoColorError::BadLen)
+        } else {
+            Self::try_from((slice[0], slice[1], slice[2]))
+        }
     }
 }
 
